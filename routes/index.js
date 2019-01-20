16428358c667374e7ef22dcffd2976a2b370fbc2
Ian Gregor <iangregor@gregorwebdesign.net.au>
2019-01-20 17:05:04 +1000

    Add mock database and /magic endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const mockDatabase = require('../lib/data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/magic', function(req, res, next) {
+  res.send(mockDatabase.getRandomMessage())
+})
 
 module.exports = router;
